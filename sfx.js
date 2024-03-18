647158c8b61773176f56da1728187af26c9e36d0
KacperTZSTI <113526043+KacperTZSTI@users.noreply.github.com>
2024-03-18 19:43:25 +0000

    fixes5

--- sfx.js
+++ sfx.js
@@ -1,52 +1,57 @@
 export var sfx = {
-    egg: new Howl({
-      src: [
-        'sfx/eg.wav'
-      ]
-    }),
-    saw1: new Howl({
-      src: [
-        'sfx/saw1.wav'
-      ]
-    }),
-    saw2: new Howl({
-      src: [
-        'sfx/saw2.wav'
-      ]
-    }),
-    saw3: new Howl({
-      src: [
-        'sfx/saw3.wav'
-      ]
-    }),
-    saw4: new Howl({
-      src: [
-        'sfx/saw4.wav'
-      ]
-    }),
-    bounce: new Howl({
-      src: [
-        'sfx/bounce.wav'
-      ]
-    }),
-    load: new Howl({
-      src: [
-        'sfx/load.wav'
-      ]
-    }),
-    loss: new Howl({
-      src: [
-        'sfx/loss.wav'
-      ]
-    }),
-    funi: new Howl({
-      src: [
-        'sfx/funy.mp3'
-      ]
-    }),
-    bird: new Howl({
-      src: [
-        'sfx/bird.mp3'
-      ]
-    }),
-  }
+  egg: new Howl({
+    src: [
+      'sfx/eg.wav'
+    ]
+  }),
+  saw1: new Howl({
+    src: [
+      'sfx/saw1.wav'
+    ]
+  }),
+  saw2: new Howl({
+    src: [
+      'sfx/saw2.wav'
+    ]
+  }),
+  saw3: new Howl({
+    src: [
+      'sfx/saw3.wav'
+    ]
+  }),
+  saw4: new Howl({
+    src: [
+      'sfx/saw4.wav'
+    ]
+  }),
+  bounce: new Howl({
+    src: [
+      'sfx/bounce.wav'
+    ]
+  }),
+  load: new Howl({
+    src: [
+      'sfx/load.wav'
+    ]
+  }),
+  loss: new Howl({
+    src: [
+      'sfx/loss.wav'
+    ]
+  }),
+  funi: new Howl({
+    src: [
+      'sfx/funy.mp3'
+    ]
+  }),
+  bird: new Howl({
+    src: [
+      'sfx/bird.mp3'
+    ]
+  }),
+  break: new Howl({
+    src: [
+      'sfx/break.mp3'
+    ]
+  }),
+}
